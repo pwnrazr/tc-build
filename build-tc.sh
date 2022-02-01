ded5f71a634edc4c36ecb3158c880a2ffef72bac
Panchajanya1999 <rsk52959@gmail.com>
2022-02-01 23:39:04 +0530

    build-tc: Switch to Thin LTO to compile LLVM
    Change-Id: If61086255fb20c43cd0b9a01ad94fd884f8b0c0e
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- build-tc.sh
+++ build-tc.sh
@@ -47,7 +47,7 @@ tg_post_msg "<code>Building LLVM</code>"
 	--clang-vendor "Azure" \
 	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3" \
 	--incremental \
-	--lto full \
+	--lto thin \
 	--projects "clang;lld;polly;compiler-rt" \
 	--pgo kernel-defconfig \
 	--shallow-clone \
