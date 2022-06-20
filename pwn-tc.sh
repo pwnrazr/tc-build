c20724ef8fa70bee91dd74f2f1cebbe6bddefc0d
Dakkshesh07 <dakkshesh5@gmail.com>
2022-06-20 12:30:52 +0800

    build-tc: Dont double quote --defines

--- pwn-tc.sh
+++ pwn-tc.sh
@@ -11,7 +11,7 @@ err() {
 
 ./build-llvm.py \
 	--clang-vendor "Razr" \
-	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3" \
+	--defines LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 \
 	--incremental \
 	--lto thin \
 	--projects "clang;lld;polly;compiler-rt" \
